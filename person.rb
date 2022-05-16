96e876019b73dcf555a10abe4139ec5c89680b65
iLynette <lynetteacholah@icloud.com>
2022-05-16 15:16:23 +0300

    Create class person,constructor, getters & setters

---
+++ person.rb
@@ -0,0 +1,10 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(name = 'Unknown', age, parent_permission: true)
+    @id = Random.rand(1...1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+ end
