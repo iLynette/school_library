055231a4471b32f8962d3431998c2edf7dc9f90f
iLynette <lynetteacholah@icloud.com>
2022-05-16 18:15:23 +0300

    Add can_use_services?

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(name = 'Unknown', age, parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1...1000)
     @name = name
     @age = age
@@ -16,6 +16,11 @@ else
 end
 
 private :is_of_age?
+
+def can_use_services?
+    is_of_age? || @parent_permission ? true : false
+ end
+end
 
 
 
