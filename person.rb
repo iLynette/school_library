81d045da09699098f5f4ef68441f6ca750b90c54
iLynette <lynetteacholah@icloud.com>
2022-05-16 18:41:35 +0300

    Fix linter error

--- person.rb
+++ person.rb
@@ -10,12 +10,12 @@ class Person
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission ? true : false
+    of_age? || @parent_permission ? true : false
   end
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 
-  private :is_of_age?
+  private :of_age?
 end
