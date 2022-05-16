55e03aec2d896bef39c254a38c23a5b680b0f2f4
iLynette <lynetteacholah@icloud.com>
2022-05-16 18:19:24 +0300

    Extend parent constructor and add classroom

--- student.rb
+++ student.rb
@@ -1,2 +1,8 @@
 require_relative './person'
 
+class Student < Person
+    def initialize(name = 'Unknown', age, parent_permission: true)
+        super(age, name, parent_permission)
+        @classroom = classroom
+    end
+end
