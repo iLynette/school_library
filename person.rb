c1d94f418c387b087d67b023258bff41ab3a6927
iLynette <lynetteacholah@icloud.com>
2022-05-16 18:17:35 +0300

    Change order so that the public method comes first

--- person.rb
+++ person.rb
@@ -9,17 +9,14 @@ class Person
     @parent_permission = parent_permission
  end
 
-def is_of_age?
-    if @age >= 18 return true
-else 
-    return false
-end
-
-private :is_of_age?
-
 def can_use_services?
     is_of_age? || @parent_permission ? true : false
  end
+ def is_of_age?
+    @age >= 18
+end
+
+private :is_of_age?
 end
 
 
