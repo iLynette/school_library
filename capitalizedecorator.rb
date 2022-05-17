08cff7ceaed261d9ba6e090270a83cf02971b882
iLynette <lynetteacholah@icloud.com>
2022-05-17 19:54:33 +0300

    Remove parenthesis from method call

--- capitalizedecorator.rb
+++ capitalizedecorator.rb
@@ -1,7 +1,7 @@
 require_relative './decorator'
 
 class CapitalizeDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name.capitalize()
-    end
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
