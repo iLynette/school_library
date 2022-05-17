bf3a29014ecc6c128c063b316c2b13ee6c46497a
iLynette <lynetteacholah@icloud.com>
2022-05-17 19:55:17 +0300

    Fix linter errors

--- decorator.rb
+++ decorator.rb
@@ -1,12 +1,12 @@
 require_relative './nameable'
 
 class BaseDecorator < Nameable
-    def initialize(nameable = Nameable.new())
-        super()
-        @nameable = nameable
-    end
+  def initialize(nameable = Nameable.new())
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
