fda095367a4458934d9e363e20ab6106e8506eb2
iLynette <lynetteacholah@icloud.com>
2022-05-18 20:00:44 +0300

    Add attr_reader for classroom

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative './person'
 
 class Student < Person
+  attr_reader :classroom
+  
   def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
