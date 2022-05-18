222674662b42af3cb8570266fffecbf5901b33a4
iLynette <lynetteacholah@icloud.com>
2022-05-18 18:23:56 +0300

    Add both way relationships

--- student.rb
+++ student.rb
@@ -9,4 +9,9 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def classroom =(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.iclude?(self)
+  end
 end
