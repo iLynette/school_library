f75d457338b626e25cdf1ea72f2946a7d80d3659
iLynette <lynetteacholah@icloud.com>
2022-05-19 08:28:39 +0300

    Include rentals in book

--- book.rb
+++ book.rb
@@ -1,8 +1,9 @@
 class Book
-    attr_accessor :title, :author
-    
+    attr_accessor :title, :author, :rentals
+
     def initialize(title, author)
         @title = title
         @author = author
+        @rentals = []
     end
 end
