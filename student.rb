d9ba1829305047f920f3919189a751b84bcce1ea
iLynette <lynetteacholah@icloud.com>
2022-05-19 09:47:42 +0300

    Fix linter errors

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative './person'
 
 class Student < Person
   attr_reader :classroom
-  
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -12,7 +12,7 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 
-  def classroom =(classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.iclude?(self)
   end
