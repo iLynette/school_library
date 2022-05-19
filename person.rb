f42cddb613cf33abbe391f98b2ac3672100b0c1e
iLynette <lynetteacholah@icloud.com>
2022-05-19 09:02:12 +0300

    Add rentals to the initialized person class

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative './nameable'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
