26b3d04c18d32742a1585c1f43324c317eb3ba2b
iLynette <lynetteacholah@icloud.com>
2022-05-19 08:51:51 +0300

    Include book and person in rental

--- rental.rb
+++ rental.rb
@@ -4,5 +4,10 @@ class Rental
 
     def initialize(date, book, person)
         @date = date
+        @book = book
+        books.rental << self
+
+        @person = person
+        person.rental << self
     end
 end
