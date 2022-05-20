554aa514abf896937493bab7c8708fffa55a1926
iLynette <lynetteacholah@icloud.com>
2022-05-20 15:05:32 +0300

    Add specialization param to teacher's file

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
