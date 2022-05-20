f71d660a79d9b6b7cfc11f6a28b819e462b442ca
iLynette <lynetteacholah@icloud.com>
2022-05-20 09:01:18 +0300

    Create a book

--- app.rb
+++ app.rb
@@ -78,4 +78,16 @@ class App
                     puts ''
                     puts "Teacher created successfully."
             end
+
+            # create a book
+            def create_book
+                print 'Title:'
+                title = gets.chomp
+                print 'Author:'
+                author = gets.chomp
+                book = Book.new(title, author)
+                @books << book
+                puts ''
+                puts 'Book created successfully'
+            end                    
 end
