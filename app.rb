6b6cb88ed5acc167d641d80ac2eb9f9fb12d44e5
iLynette <lynetteacholah@icloud.com>
2022-05-20 06:22:31 +0300

    Require the necessary files

---
+++ app.rb
@@ -0,0 +1,7 @@
+require_relative './book'
+require_relative './classroom'
+require_relative './decorator'
+require_relative './person'
+require_relative './rental'
+require_relative './student'
+require_relative './teacher'
