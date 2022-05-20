0929a384f151a2438e0da8e67f494a55ea7af445
iLynette <lynetteacholah@icloud.com>
2022-05-20 15:04:45 +0300

    Define all list selections and call main

--- main.rb
+++ main.rb
@@ -7,3 +7,27 @@ def main
   app = App.new
   app.start
 end
+
+def selection
+  case list_selections
+  when '1'
+    list_books
+  when '2'
+    list_people
+  when '3'
+    create_person
+  when '4'
+    create_book
+  when '5'
+    create_rental
+  when '6'
+    list_all_rentals
+  when '7'
+    puts 'Thank you for using the app. Goodbye!'
+  else
+    puts 'Invalid input. Try again'
+  end
+  selection
+end
+
+main
